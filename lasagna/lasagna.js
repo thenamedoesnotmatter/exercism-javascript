9803cf254c8831b7ca6a66b443624f33f80db482
nomit <timon@thenamedoesnotmatter.com>
2021-12-07 21:46:28 +0100

    reuse preparationTimeInMinutes

--- lasagna/lasagna.js
+++ lasagna/lasagna.js
@@ -66,5 +66,5 @@ export function preparationTimeInMinutes(numberOfLayers) {
  * @returns {number} the total working time
  */
 export function totalTimeInMinutes(numberOfLayers, actualMinutesInOven) {
-  return actualMinutesInOven + (PREPARATION_MINUTES_PER_LAYER * numberOfLayers);
+  return actualMinutesInOven + preparationTimeInMinutes(numberOfLayers);
 }
